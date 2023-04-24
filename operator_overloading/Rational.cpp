994380351fbd0276abe005dc7cd333eb45973c90
Val <valeriedeleon4521@gmail.com>
2023-04-24 11:30:18 -0700

    method implementation

--- operator_overloading/Rational.cpp
+++ operator_overloading/Rational.cpp
@@ -4,5 +4,17 @@
 
 Rational(int numerator, int denominator)
 {
+    assert(denominator != 0);
+    p = numerator;
+    q = denominator;
+}
 
+Rational Rational::operator*(const Rational &rhs) const
+{
+    return Rational(p * rhs.q, q * rhs.p);
+}
+
+double Rational::toDouble() const
+{
+    return static_cast<double>(p) / q;
 }

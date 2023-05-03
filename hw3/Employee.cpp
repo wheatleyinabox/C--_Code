ca192c34457afc037f661de1502d0c39f9674d6f
Val <valeriedeleon4521@gmail.com>
2023-05-03 13:32:41 -0700

    mutators & accessors for Employee.cpp

--- hw3/Employee.cpp
+++ hw3/Employee.cpp
@@ -10,7 +10,34 @@ Employee::Employee(string n, int num, int d, int m, int y)
 {
     name = n;
     number = num;
+
     Date hd = {d, m, y};
     hireDate = hd;
 }
 
+string Employee::getName()
+{
+    return name;
+}
+string Employee::getNumber() const
+{
+    return number;
+}
+Date Employee::getHireDate()
+{
+    return hr;
+}
+
+void Employee::setName(string n)
+{
+    name = n;
+}
+void Employee::setNumber(int n) const
+{
+    number = n;
+}
+void Employee::setHireDate(int d, int m, int y)
+{
+    Date newHR = {d, m, y}
+    hireDate = newHR;
+}

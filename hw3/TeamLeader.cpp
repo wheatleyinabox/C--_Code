612fc0f8027bb1bed675c3db7d238aedfdade3b2
Val <valeriedeleon4521@gmail.com>
2023-05-03 13:53:58 -0700

    constructor & mutators/accessors

--- hw3/TeamLeader.cpp
+++ hw3/TeamLeader.cpp
@@ -5,11 +5,38 @@
 
 using namespace std;
 
-TeamLeader::TeamLeader(double m, int r, int n) : ProductionWorker(s, hr)
+TeamLeader::TeamLeader(double m, int r, int n) : ProductionWorker(int s, double hr)
 {
     monthlyBonusAmount = m;
     requiredTrainingHours = r;
     numOfHoursAttended = n;
 
-    
+    shift = s;
+    hourlyRate = hr;
+}
+
+double TeamLeader::getMonthlyBonusAmount() const
+{
+    return monthlyBonusAmount;
+}
+int TeamLeader::getRequiredTrainingHours() const
+{
+    return requiredTrainingHours;
+}
+int TeamLeader::getNumOfHoursAttended() const
+{
+    return numOfHoursAttended;
+}
+
+void TeamLeader::setMonthlyBonusAmount(double m)
+{
+    monthlyBonusAmount = m;
+}
+void TeamLeader::setRequiredTrainingHours(int r)
+{
+    requiredTrainingHours = r;
+}
+void TeamLeader::setNumOfHoursAttended(int n)
+{
+    numOfHoursAttended = n;
 }

3b1c1d601c8651da4128b7e4787886f3c1b9fb74
Val <valeriedeleon4521@gmail.com>
2023-05-03 13:51:16 -0700

    start creating teamleader.cpp & removed hardcoded methods (dont need em)

---
+++ hw3/TeamLeader.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+#include <string>
+
+#include "TeamLeader.h"
+
+using namespace std;
+
+TeamLeader::TeamLeader(double m, int r, int n) : ProductionWorker(s, hr)
+{
+    monthlyBonusAmount = m;
+    requiredTrainingHours = r;
+    numOfHoursAttended = n;
+
+    
+}
--- hw3/TeamLeader.h
+++ hw3/TeamLeader.h
@@ -14,18 +14,4 @@ class TeamLeader : public ProductionWorker
         void setMonthlyBonusAmount(double m);
         void setRequiredTrainingHours(int r);
         void setNumOfHoursAttended(int n);
- // Inherited:
-        double getAnnualSalary() const;
-        double getAnnualProductionBonus() const;
-
-        void setAnnualSalary(double a);
-        void setAnnualProductionBonus(int a);
-
-        string getName();
-        string getNumber() const;
-        Date getHireDate();
-
-        void setName(string n);
-        void setNumber(int n) const;
-        void setHireDate(int d, int m, int y);
 }

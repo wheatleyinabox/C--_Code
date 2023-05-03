7196c00c37e02abab50f98d344174c18670805ce
Val <valeriedeleon4521@gmail.com>
2023-05-03 13:58:49 -0700

    driver creation/header

---
+++ hw3/mycomp.cpp
@@ -0,0 +1,23 @@
+//
+// Name:        DeLeon, Val
+// Homework:    3
+// Due:         May 5, 2023
+// Course:      cs-2560-01-sp23
+//
+// Description:
+//              FILLER
+//
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    cout << "My Company by V. DeLeon" << endl << endl;
+
+    // create objects   
+
+    return 0;
+}

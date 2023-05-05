45da7ec5882a7baba4497ca15ddbd5442cd4953b
Val <valeriedeleon4521@gmail.com>
2023-05-04 18:02:12 -0700

    user input for worker1

--- hw3/mycomp.cpp
+++ hw3/mycomp.cpp
@@ -28,9 +28,35 @@ int main()
     TeamLeader tl("Cool Dude", "152-P", 9, 12, 2016, 1, 30, 100, 0, 50);
     ShiftSupervisor ss("lazybones", "333-S", 9, 12, 2016, 30, 0.5);
 
-    // user input workers
+    // user input ONE worker - tests exception
+        ProductionWorker worker1;
+        // Sample:
+        //ProductionWorker worker1("Mark Fischbach", ***"M-628"***, 28, 6, 1989, 2, 8.70);
+        string name;
+        cout << "Please enter the employee's name: " << endl;
+        getline(cin, name);
+        worker1.setName(name);
 
-    ProductionWorker worker1("Mark Fischbach", "628-M", 28, 6, 1989, 2, 8.70);
+        string id;
+        cout << "Please enter their EID: " << endl;
+        cin >> id;
+        worker1.setNumber(id);
+
+        int month, day, year;
+        cout << "Please enter a month, day, & year for their hire date: " << endl;
+        cin >> month >> day >> year;
+        worker1.setHireDate(month, day, year);
+
+        int shift;
+        cout << "Please enter the employee's shift [1-Day 2-Night]:" << endl;
+        cin >> shift;
+        worker1.setShift(shift);
+
+        double hourlyRate;
+        cout << "Please enter the employee's hourly wage: " << endl;
+        cin >> hourlyRate;
+        worker1.setHourlyRate(hourlyRate);
+
     ProductionWorker worker2("John Doe", "123-A", 5, 1, 2023, 1, 25);
     ProductionWorker worker3("Val DeLeon", "403-W", 4, 3, 2003, 1, 25);
 

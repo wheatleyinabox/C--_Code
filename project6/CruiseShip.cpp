8f2ad94a70f5940fe7d6770a9ca267254276bff5
Val <valeriedeleon4521@gmail.com>
2023-05-08 11:21:34 -0700

    assignment header

--- project6/CruiseShip.cpp
+++ project6/CruiseShip.cpp
@@ -1,3 +1,13 @@
+//
+// Name:        DeLeon, Val
+// Project:     6
+// Due:         May 12, 2023
+// Course:      cs-2560-01-sp23
+//
+// Description:
+//              Using Programming Challenge #12, create classes Ship, CruiseShip, and CargoShip with a vitrual print function
+//              and create a dynamically allocated array of ships to then create a variety of operator overloading functions.
+//
 #include <iostream>
 #include <string>
 
--- project6/ShipApp.cpp
+++ project6/ShipApp.cpp
@@ -1,3 +1,13 @@
+//
+// Name:        DeLeon, Val
+// Project:     6
+// Due:         May 12, 2023
+// Course:      cs-2560-01-sp23
+//
+// Description:
+//              Using Programming Challenge #12, create classes Ship, CruiseShip, and CargoShip with a vitrual print function
+//              and create a dynamically allocated array of ships to then create a variety of operator overloading functions.
+//
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -12,7 +22,7 @@ int main()
 {
     cout << "Ship by V. DeLeon" << endl;
 
-    
+    // do the things :O
 
     return 0;
 }

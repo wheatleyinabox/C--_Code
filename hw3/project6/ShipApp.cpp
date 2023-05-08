47a282e20a2a3e8921ef2a28ee5829582aabfaea
Val <valeriedeleon4521@gmail.com>
2023-05-08 10:29:39 -0700

    ship driver

---
+++ hw3/project6/ShipApp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "Ship.h"
+#include "CruiseShip.h"
+#include "CargoShip.h"
+
+int main()
+{
+    cout << "Ship by V. DeLeon" << endl;
+
+    
+
+    return 0;
+}
